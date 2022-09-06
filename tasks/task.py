0f6c2469fd528ee326d0ba50593b57ea1d4eef08
Дмитрий Колб <dm.kolb@gmail.com>
2022-09-06 18:23:42 +0000

    Update tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,5 +1,5 @@
-from typing import Tuple, List
+from typing import Any, Tuple, List
 
-def get_pairs(lst: List[int]) -> List[Tuple[int, int]]:
+def get_pairs(lst: List[Any]) -> List[Tuple[Any, Any]]:
     # TODO: Add your code here
     return []

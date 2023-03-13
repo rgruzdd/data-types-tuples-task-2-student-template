12ab337ac2c521653ecc82862c7fee42fd65bbea
Gruzin <gruzd.roman@inbox.ru>
2023-03-14 00:33:09 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -2,4 +2,17 @@ from typing import Any, Tuple, List
 
 def get_pairs(lst: List[Any]) -> List[Tuple[Any, Any]]:
     # TODO: Add your code here
-    return []
+    new_list =[]
+    if len(lst) <= 1:
+        return []
+    else:
+        for i in range(0, len(lst) - 1):
+            new_l = []
+            new_l.append(lst[i])
+            new_l.append(lst[i+1])
+            new_list.append(tuple(new_l))
+
+    return new_list
+
+
+
